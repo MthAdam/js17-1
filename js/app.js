e6364366720fa43f249614053dbc87c9e5701955
MthAdam <96947337+MthAdam@users.noreply.github.com>
2022-02-19 01:57:17 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+/////////mathematical operation
+
+//addition//
+ function sum(numb1,numb2){
+     document.write(numb1+numb2+'<br>')
+ }
+ sum(12,24) 
+//substraction//
+ function sub(numb1,numb2){
+     document.write(numb1-numb2+'<br>')
+ }
+ sub(24,12)
+
+ //multiplication//
+ function mult(numb1,numb2){
+     document.write(numb1/numb2+'<br>')
+ }
+ mult(12,6)
+
+ //exponentiation//
+ function exp(numb1,numb2){
+     document.write(numb1**numb2+'<br>')
+ }
+ exp(2,8)
+
+ //divition//
+ function div(numb1,numb2){
+     document.write(numb1/numb2+'<br>')
+ }
+ div(12,6)
+
+ //modulus//
+ function mod(numb1,numb2){
+     document.write(numb1%numb2+'<br>')
+ }
+ mod(14,6)
